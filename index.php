18acc9d376e6c8b3e8816f30a7ad79a3f8471998
Chidubem Nwodo <nwodochodubem1@gmail.comgit config --global user.email nwodochidubem1@gmail.com>
2020-03-26 05:05:14 +0400

    php task

--- index.php
+++ index.php
@@ -6,7 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         
     <title>Contact Me</title>
-    
+    </head>
+    <body>
     <form action="check.php" method="POST">
            
     		<p>
